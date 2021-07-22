d50c2fee660097c499dd0e391a0d52b611211a5c
Thumper <shaunwhl1991@gmail.com>
2021-07-22 20:01:47 +1000

    Add required packages and set target URL variable

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Required Packages */
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+/* Const variables */
+const targetUrl = 'https://www.coronavirus.vic.gov.au/who-can-get-vaccinated';

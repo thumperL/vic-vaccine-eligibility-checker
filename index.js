624d1756dcd213e256a0b5a57b288bc2b3d264cf
Thumper <shaunwhl1991@gmail.com>
2021-07-22 23:05:28 +1000

    add promise to return saved eligible text if file exists, or else create it

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const fs = require('fs');
 
 /* Const variables */
 const targetUrl = 'https://www.coronavirus.vic.gov.au/who-can-get-vaccinated';
+const existingEligibilityFilePath = 'eligible_list.txt';
+
+
+
+// check file exists, if yes return data
+const existingFilePromise = new Promise((resolve, reject) => {
+  try {
+    fs.readFile(existingEligibilityFilePath, 'utf8', async (err, data) => {
+      if (err) {
+        await fs.writeFile(existingEligibilityFilePath, '', ()=>{});
+        return resolve('');
+      } else {
+        return resolve(data);
+      }
+    })
+  } catch {
+    reject('Get Existing File Error');
+  }
+});
 
 request(targetUrl)
   .then((html) => {

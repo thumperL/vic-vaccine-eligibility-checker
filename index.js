791fa325d32038a6284575b4a8889a195e7e263e
Thumper <shaunwhl1991@gmail.com>
2021-07-22 20:02:20 +1000

    get target content to process later
    get target content to process later

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const fs = require('fs');
 
 /* Const variables */
 const targetUrl = 'https://www.coronavirus.vic.gov.au/who-can-get-vaccinated';
+
+request(targetUrl)
+  .then((html) => {
+    const $ = cheerio.load(html);
+    const eligible_content = $('.rpl-markup__inner');
+  }) 
+  .catch((err) => console.log(err));

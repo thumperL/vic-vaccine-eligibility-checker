c4be44ad1ff9efdb03febc8f2f59dde22ddf586f
Thumper <shaunwhl1991@gmail.com>
2021-07-24 16:59:33 +1000

    add mailgun and promisify the writefile and email actions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const cron = require('node-cron');
+const mailgun = require('mailgun-js');
 const { resolve } = require('path');
 
 cron.schedule('*/15 * * * *', async () => {
@@ -43,19 +44,35 @@ cron.schedule('*/15 * * * *', async () => {
     .catch((err) => reject('Getting VIC GOV live data failed'));
   });
 
-  Promise.all([existingFilePromise, liveListPromise]).then(([existsEligibleArr, liveEligibleArr]) => {
+  Promise.all([existingFilePromise, liveListPromise])
+  .then(([existsEligibleArr, liveEligibleArr]) => {
     const newEligibleArr = liveEligibleArr.filter((x) => !existsEligibleArr.includes(x));
-
     // If there is any difference
     if (newEligibleArr.length !== 0) {
       const today = new Date();
       const dateStr = today.getFullYear().toString() + (today.getMonth() + 1).toString().padStart(2, '0') + today.getDay().toString().padStart(2, '0') + '_' + today.getHours().toString().padStart(2, '0') + today.getMinutes().toString().padStart(2, '0');
-      const writeFiles = (async (dateStr) => {
-        let newFile = (existsEligibleArr.length === 0)? true : await fs.promises.writeFile('added_' + dateStr + '.txt', JSON.stringify(newEligibleArr)).then(()=>true);
-        let overrideFile =  await fs.promises.writeFile('eligible_list.txt', JSON.stringify(liveEligibleArr)).then(()=>true);
-        return (newFile && overrideFile);
-      }).catch((err)=>console.error(err));
-      writeFiles(dateStr);
+      const writeFileNew = new Promise((resolve, reject) => {
+        resolve((existsEligibleArr.length === 0)? true : fs.promises.writeFile('added_' + dateStr + '.txt', JSON.stringify(newEligibleArr)).then(()=>true));
+      });
+      const writeFileOverride = new Promise((resolve, reject) => {
+        resolve(fs.promises.writeFile('eligible_list.txt', JSON.stringify(liveEligibleArr)).then(()=>true));
+      });
+      const notify = new Promise((resolve, reject) => {
+        const DOMAIN = "";
+        const mg = mailgun({apiKey: process.env.EMAIL_API, domain: process.env.EMAIL_DOMAIN});
+        const data = {
+          from: process.env.EMAIL_FROM,
+          to: process.env.EMAIL_TO,
+          subject: "New Eligible list Changes",
+          html: `<pre style="white-space:initial"> <pre>${JSON.stringify(newEligibleArr)}</pre> </pre>`
+        };
+        mg.messages().send(data, (error, body) => {(error)? reject('[Email Error]\r\n' + error) : resolve('[Email Sent]')});
+      });
+      Promise.all([writeFileNew, writeFileOverride, notify]).then(() => {
+        console.log('Notified and Completed');
+      })
+    } else {
+      console.log('Completed');
     }
   });
 });
